69b6f682280d44f8178e253ec573c102bb947811
sebastian <sebastiandevincentis72@gmail.com>
2024-04-26 04:23:16 -0300

    mongodb(13:56hs)

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import morgan from 'morgan';
+
+
+const app = express();
+
+app.use(morgan('dev'));
+
+export default app;
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import app from './App.js';
+
+app.listen(4000);
+console.log("Server on port", 4000);
+
