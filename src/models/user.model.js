626245b514928947fd990be989c0e853c5f0fc15
sebastian <sebastiandevincentis72@gmail.com>
2024-04-27 03:45:39 -0300

    crecion de token (39:42hs)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
      username:{
         type:String,
         required:true,
@@ -16,6 +16,8 @@ const userSchema = mongoose.Schema({
         type:String,
         required:true,
      }
+},{
+   timestamps:true
 });
 
 export default mongoose.model("User",userSchema);
