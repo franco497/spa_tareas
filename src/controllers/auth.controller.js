6653d56fbd3cd9ec01f3c5fa5ac49b48a72b9ae3
sebastian <sebastiandevincentis72@gmail.com>
2024-04-29 03:36:16 -0300

    (47:39hs) generacion de token funcionando

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,41 @@
 import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
  
 export const register = async (req,res) => {
     const {email,password,username} = req.body;
 
     try {
+
+        const passwordHashs = await bcrypt.hash(password, 10);
+
         const newUser = new User({
             username,
             email,
-            password
+            password:passwordHashs,
         });
         
         const UserSaved = await newUser.save();
-         res.json(UserSaved);
+
+        jwt.sign({
+            id:UserSaved._id, 
+        },"secret123",
+        {
+            expiresIn:"1d"
+        },
+        (err, token) => {
+            if(err) console.log(err);
+            res.json({token});
+        });
+
+        // res.json({
+        //    id:UserSaved._id,
+        //    username:UserSaved.username,
+        //    email:UserSaved.email,
+        //    createdAt:UserSaved.createdAt,
+        //    updatedAt:UserSaved.updatedAt,
+        // });
     } catch (error) {
         console.log(error);
     }
