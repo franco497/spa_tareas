5b22c4fa267602a383339e353269bcb263c7595d
sebastian <sebastiandevincentis72@gmail.com>
2024-05-22 08:32:52 -0300

    finalizacion del back end (1:57:47hs)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -81,3 +81,4 @@ export const profile = async (req,res) => {
 
     res.send("profile");
 }
+
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,23 +1,30 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
 import {
-    getTasks,
-    getTask,
-    createTask,
-    updateTask,
-    deleteTask,
-} from '../controllers/tasks.controllers.js';
+  getTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/tasks.controllers.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/tasks.schema.js";
 
 const router = Router();
 
-router.get('/tasks',authRequired, getTasks);
+router.get("/tasks", authRequired, getTasks);
 
-router.get('/tasks/:id',authRequired, getTask);
+router.get("/tasks/:id", authRequired, getTask);
 
-router.post('/tasks',authRequired, createTask);
+router.post(
+  "/tasks",
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTask
+);
 
-router.delete('/tasks/:id',authRequired, deleteTask);
+router.delete("/tasks/:id", authRequired, deleteTask);
 
-router.put('/tasks/:id',authRequired,updateTask);
+router.put("/tasks/:id", authRequired, updateTask);
 
 export default router;
