1226718e3173dd3da020ac2aa0214f9f3eac7ad2
sebastian <sebastiandevincentis72@gmail.com>
2024-05-25 06:45:16 -0300

    registro de usuario (2:05:47hs)

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>Tasks page</h1>} />
+        <Route path="/add-tasks" element={<h1>new tasks</h1>} />
+        <Route path="/tasks/:id" element={<h1>update task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
