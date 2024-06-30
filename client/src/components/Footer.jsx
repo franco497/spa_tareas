b8cc984c6462a16ec6c9b27abe9f2fc24530e3d3
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-30 01:23:06 -0400

    (Rigoberto)Edit: Footer(maqueta Footer 100%)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,17 +6,15 @@ const Footer = () => {
   const Year = new Date().getFullYear();
 
   return (
-    <footer className="relative bg-black text-white">
+    <footer className="relative bg-lime-900 text-yellow-50 transition">
       <div className="relative top-0 left-0 w-[100%] overflow-hidden">
         <svg data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1200 120"
           preserveAspectRatio="none">
           <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,
-                        250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,
-                        3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="relative block h-[600px] fill-white"></path>
+            d="M1200 0L0 0 598.97 114.72 1200 0z"
+            className="relative block h-[600px] fill-[#b9f8f4]"></path>
         </svg>
         <div className="grid lg:grid-cols-4 gap-20 sm:grid-cols-1 p-20">
           <div className="flex flex-col gap-5">
@@ -95,3 +93,53 @@ const Footer = () => {
 };
 
 export default Footer;
+
+/*
+<div class="custom-shape-divider-top-1719722421">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
+    </svg>
+</div>
+
+--
+.custom-shape-divider-top-1719722421 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-top-1719722421 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 120px;
+}
+
+.custom-shape-divider-top-1719722421 .shape-fill {
+    fill: #B0EC8A;
+}
+
+// For tablet devices
+@media (min-width: 768px) and (max-width: 1023px) {
+    .custom-shape-divider-top-1719722421 svg {
+        width: calc(100% + 1.3px);
+        height: 88px;
+    }
+}
+
+/// For mobile devices
+@media (max-width: 767px) {
+    .custom-shape-divider-top-1719722421 svg {
+        width: calc(100% + 1.3px);
+        height: 91px;
+    }
+}
+
+
+
+
+
+*/
