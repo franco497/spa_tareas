4e7444ba94250f990cc883ca7c4734159d73c0c0
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-29 23:08:49 -0400

    (Rigoberto)edit:App(ajustes finales antes de prueba con crateBrowseRouter&&RouterProvider)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -45,17 +45,7 @@ function App() {
     }*/
   ]);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>HomePage page</h1>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/tasks" element={<h1>Tasks page</h1>} />
-        <Route path="/add-tasks" element={<h1>new tasks</h1>} />
-        <Route path="/tasks/:id" element={<h1>update task</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
