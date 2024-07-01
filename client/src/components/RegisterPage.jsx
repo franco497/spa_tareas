0e09a2046d2cb31f93f7993fa8c010fab6b4a8a0
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-07-01 00:01:38 -0400

    (Rigoberto)edit:RegisterPage(modificacion del formulario a nivel de etiquetas y de useForm)

--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -1,36 +1,48 @@
 import { useForm } from "react-hook-form";
-import { registerRequest } from "../api/auth.js";
+//import { registerRequest } from "../api/auth.js";
 
 const RegisterPage = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { error } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+
 
   return (
     <div className="bg-zinc-800 max-w-md P-10 rounded-md">
-      <form
-        onSubmit={handleSubmit( async(values) => {
-          console.log(values);
-          const res = await registerRequest(values);
-          console.log(res);
-        })}
-      >
-        <input
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input id="username"
           type="text"
-          {...register("username", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           placeholder="Username"
-        />
-        <input
+          {...register("username", { required: "the username is required" })}
+          />
+          {error.username && <p>{error.username.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email"
           type="email"
-          {...register("email", { required: true })}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          {...register("password", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           placeholder="Password"
-        />
+          {...register("email", { required: "the email is required" })}
+          />
+          {error.email && <p>{error.email.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id="password"
+           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Password"
+          {...register("password", { required: "the password is required" })}
+          />
+          {error.password && <p>{error.password.message}</p>}
+        </div>
+          
         <button type="submit">Register</button>
       </form>
     </div>
@@ -38,3 +50,11 @@ const RegisterPage = () => {
 }
 
 export default RegisterPage;
+
+/*
+ {handleSubmit( async(values) => {
+          console.log(values);
+          const res = await registerRequest(values);
+          console.log(res);
+        })}
+*/
