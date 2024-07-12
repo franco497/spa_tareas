ed3ba8ad88e836a359dfa58039e469733837d497
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-07-11 21:47:17 -0400

    (Rigoberto)Edit:LoginPage(from cambio de user por email)

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -26,13 +26,13 @@ const LoginPage = () => {
           <h2 className="text-2xl font-bold lg:my-12 my-3 text-center">Login</h2>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <div className="flex flex-col mx-6">
-              <label htmlFor="username" className="text-lg font-medium">Username</label>
+              <label htmlFor="email" className="text-lg font-medium">Email</label>
               <input
-                id="username"
-                type="text"
+                id="email"
+                type="email"
                 className="border border-gray-300 text-yellow-800 px-4 py-2 rounded-md my-2"
-                placeholder="Username"
-                {...register("username", { required: "The username is required" })}
+                placeholder="email"
+                {...register("email", { required: "The email is required" })}
               />
               {errors.username && <p className="text-red-600">{errors.username.message}</p>}
             </div>
