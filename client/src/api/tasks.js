772a88ac84f33e1190308e65cb4702d92b51f196
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-07-20 13:48:37 -0400

    (rigoberto)new:tasks.js(func para interactuar con el back)

---
+++ client/src/api/tasks.js
@@ -0,0 +1,6 @@
+import instanceAxios from "./axios";
+
+export const getTasksRequest = () => instanceAxios.get('/tasks');
+export const createTaskRequest = (task) => instanceAxios.post('/tasks', task);
+export const updateTaskRequest = (id, task) => instanceAxios.put(`/tasks/${id}`, task);
+export const deleteTaskRequest = (id) => instanceAxios.delete(`/tasks/${id}`);
