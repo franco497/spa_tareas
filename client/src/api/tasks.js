f9471827c23b39bf6319d4000d7639ce15d149d4
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-07-20 23:48:56 -0400

    (atlas11)edit:tasks.js(ajuste de la solicitud axios para getTaskRequest; crud al 90%)

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,7 +1,7 @@
 import instanceAxios from "./axios";
 
 export const getTasksRequest = () => instanceAxios.get('/tasks');
-export const getTaskRequest = () => instanceAxios.get('/tasks/:id');
+export const getTaskRequest = (id) => instanceAxios.get(`/tasks/${id}`);
 export const createTaskRequest = (task) => instanceAxios.post('/tasks', task);
 export const updateTaskRequest = (id, task) => instanceAxios.put(`/tasks/${id}`, task);
 export const deleteTaskRequest = (id) => instanceAxios.delete(`/tasks/${id}`);
